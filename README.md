6987aa129ebfc63b7a9c3bbf48b714fe728ffd61
jackietng <jn1thythy56@gmail.com>
2024-09-30 19:40:40 -0700

    updated index.js, generateMarkdown.js, package.json and README.md

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ReadMe-Generator
-Module 7 Challenge - Professional ReadMe Generator
+by Jacqueline Nguyen
+![Github license](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
+## Table of Contents
+* [Description](#description)
+* [Requirements](#requirements)
+* [Usage](#usage)
+* [Contact-Me](#contact-me)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Demo](#demo)
+
+* [License](#license)
+
+## Description
+This ReadMe generator was created by using NodeJS and Inquirer (an NPM product). This app automatically generates professional ReadMe's.
+## Requirements
+Inquirer 8.2.4 (or later)
+## Usage
+Markdown, JavaScript, Node, JSON
+## Contact-Me
+* Name - Jacqueline Nguyen
+* Email - jn1thythy56@gmail.com
+* Github - [jackietng](https://github.com/jackietng/)
+## Contributors
+N/A
+## Testing
+```
+N/A
+```
+## Demo
+
+## License
+
+        Licensed under the MIT license.
--- index.js
+++ index.js
@@ -1,14 +1,89 @@
 // TODO: Include packages needed for this application
-
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer.prompt([
+        {
+          type: 'input',
+          message: 'What would you like your repository title to be?',
+          name: 'title',
+        },
+        {
+          type: 'input',
+          message: 'What would you like your project name to be?',
+          name: 'name',
+        },
+        {
+          type: 'input',
+          message: 'Provide a description of your application:',
+          name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the devlopment environment running.',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples for use. Include screenshots as needed.',
+            name: 'usage',
+        },
+        {
+          type: 'input',
+          message: 'How would you test this project?',
+          name: 'test',
+        },       
+        {
+            type: 'input',
+            message: 'List your collaborates',
+            name: 'credits',
+        },
+        {
+            type: 'list',
+            message: 'Choose your license for your project.',
+            name: 'license',
+            choices: [
+                { value: 'Apache' },   
+                { value: 'BSD3' },  
+                { value: 'LGPL' },  
+                { value: 'MIT' },  
+                { value: 'MPL' }, 
+                { value: 'None' }, 
+            ]
+        },
+        {
+            type: 'input',
+            message: 'Enter your GitHub username:',
+            name: 'github',
+        },
+        {
+          type: 'input',
+          message: 'Enter your email:',
+          name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'Where is this application deployed at?',
+        name: 'deploy',
+    },
+      ])
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating Professional README.md File...");
+        writeToFile(".dis/README.md", generateMarkdown({...responses}));
+    });
+}
 
 // Function call to initialize app
 init();
